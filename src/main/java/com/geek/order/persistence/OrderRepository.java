3ad588631f6e0511d1a6e09389780a3e869a8d25
Debri <18908356464@163.com>
2017-05-23 21:27:05 +0800

    completed

--- src/main/java/com/geek/order/persistence/OrderRepository.java
+++ src/main/java/com/geek/order/persistence/OrderRepository.java
@@ -14,8 +14,10 @@ public interface OrderRepository extends CrudRepository<Order, Long> {
     @Query(value = "update order set trade_id = ?1 ,trade_status=1 where id =?2 ", nativeQuery = true)
     public void updateOrderByTrade(Long tradeId, Long id);
 
+    @Query(value = "select  * form order o where user_id=?1", nativeQuery = true)
     public Iterable<Order> findByUserId(Long userId);
-    @Query(value = "select * from order" ,nativeQuery = true)
+
+    @Query(value = "select * from order", nativeQuery = true)
     public Iterable<Order> find();
 
 }
